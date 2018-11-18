c9ebdeaa2534683d66f94a9007326596905bd046
djknit <djknit@gmail.com>
2018-11-18 13:13:13 -0600

    JawsDB

--- config/connection.js
+++ config/connection.js
@@ -1,11 +1,17 @@
 const mysql = require("mysql");
+let connection;
 
-const connection = mysql.createConnection({
-    port: 3306,
-    user: "root",
-    password: process.env.MY_SQL_PASSWORD ? process.env.MY_SQL_PASSWORD : "",
-    database: "burgers_db"
-});
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+}
+else {
+    connection = mysql.createConnection({
+        port: 3306,
+        user: "root",
+        password: process.env.MY_SQL_PASSWORD ? process.env.MY_SQL_PASSWORD : "",
+        database: "burgers_db"
+    });
+}
 
 connection.connect(error => {
     if (error) return console.error(error);
